3766666e779850f431c353127e502ccec1a34f62
chienbui09 <buiducchientlf2209@gmail.com>
2024-03-03 13:35:42 +0700

    init more projects

---
+++ book-management/src/main/java/com/personal/BookManagement.java
@@ -0,0 +1,7 @@
+package com.personal;
+
+public class BookManagement {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ employee-management/src/main/java/com/personal/EmployeeManagement.java
@@ -0,0 +1,7 @@
+package com.personal;
+
+public class EmployeeManagement {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
