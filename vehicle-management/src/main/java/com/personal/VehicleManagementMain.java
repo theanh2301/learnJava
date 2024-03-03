e79a77e1d790ed94f058b76d7319f33bc750a1c7
chienbui09 <buiducchientlf2209@gmail.com>
2024-03-03 12:16:14 +0700

    init Vehicle Management module

---
+++ vehicle-management/src/main/java/com/personal/VehicleManagementMain.java
@@ -0,0 +1,7 @@
+package com.personal;
+
+public class VehicleManagementMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ vehicle-management/src/main/java/com/personal/model/Vehicle.java
@@ -0,0 +1,5 @@
+package com.personal.model;
+
+public abstract class Vehicle {
+
+}
